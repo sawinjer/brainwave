05a126933c9873fa4b20d698be1dc36ede6b1d5e
sawinjer <polisukandrij68@gmail.com>
2026-02-14 16:11:32 +0200

    biome

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,8 @@
-import { Elysia, t } from "elysia";
+import { Elysia } from "elysia";
 
 const app = new Elysia()
-  .get("/", () => "Hello")
-  .get("/health", () => ({ status: "ok" }));
+	.get("/", () => "Hello")
+	.get("/health", () => ({ status: "ok" }));
 
 export type App = typeof app;
 export { app };
-export { t };
