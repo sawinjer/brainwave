5c8b231eadb6901532074bb833d29c6064261bf5
sawinjer <polisukandrij68@gmail.com>
2026-02-14 19:56:17 +0200

    formated

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from './components/ui/button';
+
 export function App() {
 	return (
-		<div className="dark w-screen h-screen bg-background flex items-center justify-center">
-			<h1 className="text-foreground">Hello there</h1>
+		<div className="dark w-screen h-screen bg-background flex flex-col gap-3 items-center justify-center">
+			<h1 className="text-4xl font-serif text-foreground">Hello there, in BrainWave 🧠</h1>
+			<div className="flex gap-2 justify-center">
+				<Button>Create a quiz</Button>
+				<Button>Join a quiz</Button>
+				<Button>Specteta a quiz</Button>
+			</div>
 		</div>
 	);
 }
