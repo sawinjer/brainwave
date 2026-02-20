3dce165ff67c65fdc5d5a891b2086e12ba0bb550
sawinjer <polisukandrij68@gmail.com>
2026-02-20 11:39:53 +0200

    fix

--- packages/server/src/env.ts
+++ packages/server/src/env.ts
@@ -1,10 +1,10 @@
-import z from 'zod';
-import 'dotenv/config';
+import z from "zod";
+import "dotenv/config";
 
 export const env = z
-	.object({
-		DATABASE_URL: z.string(),
-		REDIS_URL: z.string(),
-		KAFKA_BROKERS: z.string().default('localhost:9092'),
-	})
-	.parse(process.env);
+  .object({
+    DATABASE_URL: z.string(),
+    REDIS_URL: z.string(),
+    KAFKA_BROKERS: z.string(),
+  })
+  .parse(process.env);
