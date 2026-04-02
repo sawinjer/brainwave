30e9fe12feadc6441ba13944a8bbe7f3844b49b1
sawinjer <polisukandrij68@gmail.com>
2026-04-02 20:32:49 +0300

    fetch data

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,6 +1,16 @@
+import { server } from "@/lib/server";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const [data, setData] = useState<unknown>();
+
+  useEffect(() => {
+    server.health.get().then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
     <View
       style={{
@@ -9,7 +19,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Data:</Text>
+      <Text>{JSON.stringify(data)}</Text>
     </View>
   );
 }
---
+++ apps/mobile/lib/server.ts
@@ -0,0 +1,6 @@
+import type { App } from "@brainwave/server/client";
+import { treaty } from "@elysiajs/eden";
+
+const serverUrl = process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000";
+
+export const server = treaty<App>(serverUrl);
